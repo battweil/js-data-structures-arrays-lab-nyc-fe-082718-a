710f3b8bf54ce4873deb91f27d47283d63eb596e
Cricky Cicchetti <cricky.cicchetti@gmail.com>
2018-10-08 00:50:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+drivers.push("Ralph")
+
+drivers;
+
+// drivers.unshift("Bob");
+
+// drivers.pop();
+
+// drivers.shift();
+
+// drivers.slice(2,1,"Garfunkel");
